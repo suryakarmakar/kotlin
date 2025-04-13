c60db846059a5cd45da9858650f2979773b18b47
SuryaKarmakar <suryakarmakar.development@gmail.com>
2025-04-13 15:06:04 +0530

    exercise - user input

---
+++ exercise/02UserInput.kt
@@ -0,0 +1,6 @@
+fun main() {
+    /* write a program that asks the user for a number and prints out that number multiplied by 5 */
+    print("please enter a number: ")
+    val number: Int = readln().toInt()
+    println("$number * 5 = ${number * 5}")
+}
